7acd404dec08d76e2c8595e0be25025ec897372d
AshenRain <49451892+AshenRain@users.noreply.github.com>
2022-05-09 13:49:30 +0300

    Add files via upload

---
+++ ТеstYandex2_3.py
@@ -0,0 +1,43 @@
+import copy
+'''
+a = input().split()
+k = int(input())
+c = set(a) #уникальные стороны кубика
+'''
+
+
+def generation(q, depth, num = ''):
+    if depth != 0:
+        for i in q:
+            generation(q, depth - 1, num + i)
+    else:
+        num_tmp = copy.deepcopy(num)
+        p = 1
+        sum = 0
+        num = list(num)
+        for j in range(k-1,0,-1):
+            if num[j] == num[j-1]: 
+                num.pop(j) #удаление повторящющихся эл-ов
+        for j in num:
+            sum += int(j) #сумирование оствашихся цифр для мат. ожид.
+        if sum not in m:
+            for j in range(k): 
+               p*= prob[num_tmp[j]] #рассчет вероятностей события 
+            m[sum] = [p] #общая вероятность для данной суммы
+            m[sum].append(p) #вероятность для одного такого выпадения
+        else:
+             m[sum][0] += m[sum][1] # сделано, чтобы повторно не рассчитывать вероятность для той же суммы
+
+k = 2
+c = ['1','2','3','4','5','6']
+a = ['1','2','3','4','5','6']
+
+m = dict()
+prob = dict() #вероятности сторон кубика
+for i in a: prob[i] = prob.get(i,0) + 1/6
+generation(c,k)#генерация уникальных комбинаций
+
+mat_o = 0
+for k,v in m.items():
+    mat_o += k*v[0]
+print(mat_o) 
