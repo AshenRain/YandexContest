b2d2a83f03bf9734ff248e85bd280f4b5244dafa
AshenRain <49451892+AshenRain@users.noreply.github.com>
2022-05-27 09:49:55 +0300

    Add files via upload

--- Task_E/TestYandexE_clear.py
+++ Task_E/TestYandexE_clear.py
@@ -8,6 +8,7 @@ def input_points_train(stroki):
     tmp = []
     for i in range(1000):
         tmp = list(map(float, stroki[i].split(sep = '\t')))
+        #tmp[:5] = [x*100 for x in tmp[:5]]
         train_x.append(tmp[:5])
         train_y.append(tmp[-1])
     return train_x, train_y
@@ -22,13 +23,25 @@ fin.close
 x,y = input_points_train(text_line[:1000])
 x,y = np.array(x), np.array(y)
 lin_reg = LinearRegression()
-poly_reg = PolynomialFeatures(degree=5)
+poly_reg = PolynomialFeatures(degree=3)
 poly_x = poly_reg.fit_transform(x)
 lin_reg.fit(poly_x,y)
 
-x_val, y = input_points_train(text_line[1000:])
-x_val = np.array(x_val)
+x_val, yy = input_points_train(text_line[1000:])
+print('Before np.array',x_val[0])
+np.set_printoptions(precision=20)
+x_val = np.array(x_val, dtype=np.float64)
+print('After np.array ',x_val[0])
 x_pol = poly_reg.fit_transform(x_val)
+tmp = x_val[0][0] * x_val[0][1] * x_val[0][1]+ x_val[0][2]* x_val[0][3] + x_val[0][4]
+# y = x1 * x2 * x2 + x3 * x4 + x5
+print('tmp ', tmp)
+print('     {:.6f}'.format(lin_reg.predict([x_pol[0]])[0]))
+print(tmp - lin_reg.predict([x_pol[0]])[0])
 
-for i in range(1000):
-    print('{:.6f}'.format(lin_reg.predict([x_pol[i]])[0]))
+tmp = x_val[1][0] * x_val[1][1] * x_val[1][1]+ x_val[1][2]* x_val[1][3] + x_val[1][4]
+print('tmp ', tmp)
+print('     {:.6f}'.format(lin_reg.predict([x_pol[1]])[0]))
+print(tmp - lin_reg.predict([x_pol[1]])[0])
+#for i in range(1000):
+ #   print('{:.6f}'.format(lin_reg.predict([x_pol[i]])[0]))
