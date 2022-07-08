e8f6057170719867c6637c38f6df09d340af8f82
AshenRain <itprofi46@gmail.com>
2022-07-08 06:59:25 +0300

    Update Yandex2_Task_E.py

--- ML_Internship_2022_Spring-Summer/Task_E/Yandex2_Task_E.py
+++ ML_Internship_2022_Spring-Summer/Task_E/Yandex2_Task_E.py
@@ -1,6 +1,8 @@
 import numpy as np
+from math import tan, sin, cos
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+
 #from sklearn.preprocessing import SplineTransformer
 
 
@@ -16,23 +18,23 @@ tmp = []
 n = text_line[0]
 for line in text_line[1:]:
    tmp = list(map(float, line.split()))
-   train_x.append([tmp[0]])
+   train_x.append([tan(tmp[0]), sin(tmp[0])**2, 2*cos(tmp[0])*sin(tmp[0]), cos(tmp[0])**2, tmp[0]**0.5])
    train_y.append([tmp[1]])
 
 
 x,y = np.array(train_x), np.array(train_y)
 #x,y = train_x, train_y
 lin_reg = LinearRegression()
-poly_reg = PolynomialFeatures(degree=10)
+#poly_reg = PolynomialFeatures(degree=2)
 #poly_reg = SplineTransformer(degree = 1, n_knots = 4, knots = 'quantile')
-poly_x = poly_reg.fit_transform(x)
+#poly_x = poly_reg.fit_transform(x)
 #print(poly_x)
-lin_reg.fit(poly_x,y)
-print('score ',lin_reg.score(poly_x,y))
+lin_reg.fit(x,y)
+print('score ',lin_reg.score(x,y))
 print(lin_reg.coef_)
+print(len(lin_reg.coef_[0]))
 
 
-"""
+
 for i in lin_reg.coef_[0]:
     print("{:2f}".format(i), end = ' ')
-"""
