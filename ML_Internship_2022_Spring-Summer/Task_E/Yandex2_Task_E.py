7cda73434b9d3712e728bb1c4643b0e1d15a50ef
AshenRain <itprofi46@gmail.com>
2022-07-08 05:27:42 +0300

    Create Yandex2_Task_E.py

---
+++ ML_Internship_2022_Spring-Summer/Task_E/Yandex2_Task_E.py
@@ -0,0 +1,38 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+#from sklearn.preprocessing import SplineTransformer
+
+
+fin = open('input.txt', 'r', encoding='utf8')
+text_line = []
+for line in fin:
+    text_line.append(line)
+fin.close
+
+train_x = []
+train_y = []
+tmp = []
+n = text_line[0]
+for line in text_line[1:]:
+   tmp = list(map(float, line.split()))
+   train_x.append([tmp[0]])
+   train_y.append([tmp[1]])
+
+
+x,y = np.array(train_x), np.array(train_y)
+#x,y = train_x, train_y
+lin_reg = LinearRegression()
+poly_reg = PolynomialFeatures(degree=10)
+#poly_reg = SplineTransformer(degree = 1, n_knots = 4, knots = 'quantile')
+poly_x = poly_reg.fit_transform(x)
+#print(poly_x)
+lin_reg.fit(poly_x,y)
+print('score ',lin_reg.score(poly_x,y))
+print(lin_reg.coef_)
+
+
+"""
+for i in lin_reg.coef_[0]:
+    print("{:2f}".format(i), end = ' ')
+"""
