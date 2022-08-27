b2cd43edc89d7d2e41055096ce7f8326c0765186
AshenRain <itprofi46@gmail.com>
2022-08-27 04:59:40 +0300

    add AUC_6

--- ML_Internship_Day_2022_Summer/AUC_6.py
+++ ML_Internship_Day_2022_Summer/AUC_6.py
@@ -1,13 +1,3 @@
-
-
-![Image alt](https://github.com/AshenRain/YandexContest/raw/main/ML_Internship_Day_2022_Summer/1.jpg)
-
-![Image alt](https://github.com/AshenRain/YandexContest/raw/main/ML_Internship_Day_2022_Summer/2.jpg)
-
-
-Код:
-
-```
 def mergeSortInversions(arr):
     if len(arr) == 1:
         return arr, 0
@@ -64,68 +54,21 @@ t,inv = mergeSortInversions([x[1] for x in t])
 # Финальный подсчет
 auc = (pair_all - inv - 0.5*pair_equal)/pair_all
 print('{:.6f}'.format(auc))
-```
+
+"""
+4
+1 0.1
+1 0.4
+2 0.35
+2 0.8
+
+0.75
 
 
-
-Пример 1
-
-```
-Ввод:
 2
-0 0
 1 1
-
-Вывод:
-1.000000
-```
-
-Пример 2
-
-```
-Ввод:
-2
-0 1
-1 0
-
-Вывод:
-0.000000
-```
-
-Пример 3
-
-```
-Ввод:
-3
-0.5 0
-0.5 1
-2 0.5
-
-Вывод:
-0.500000
-```
-
-Пример 4
-
-```
-Ввод:
-7
 0 0
-0 1
-1 2
-0 3
-1 4
-1 5
-1 6
 
-Вывод:
-0.916667
-```
-
-Пример 5
-
-```
-Ввод:
 10
 0 4
 3 0
@@ -138,26 +81,5 @@ print('{:.6f}'.format(auc))
 4 4
 0 0
 
-Вывод:
 0.538462
-```
-
-Пример 6
-
-```
-Ввод:
-10
-3 4
-3 4
-2 1
-3 2
-4 1
-0 2
-2 0
-1 2
-4 4
-3 3
-
-Вывод:
-0.648649
-```
+"""
